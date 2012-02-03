8e1d310d1d07147d7864a6253c90a452d3d16925
James A. Rosen <james@zendesk.com>
2012-02-03 12:07:15 -0800

    adding apps from Lotus

---
+++ bookmarks/app.js
@@ -0,0 +1,81 @@
+//= require ./app.js
+
+(function(exports) {
+
+  Zendesk.Apps.BookmarksApp = Zendesk.Apps.App.extend({
+    location: Zendesk.Apps.Site.TICKET_PROPERTIES,
+    appID: '/apps/01-bookmarks/versions/1.0.0',
+    name: 'Bookmarks',
+    defaultSheet: 'loading',
+
+    templates: {
+      main: '<div data-sheet-name="loading" class="loading">' +
+            '  <h3>Bookmarks</h3>' +
+            '  Loading&hellip;' +
+            '</div>' +
+            '<div data-sheet-name="bookmarks">' +
+            '  <h3>Bookmarks</h3>' +
+            '  <ul>' +
+            '    {{#bookmarks}}<li>' +
+            '      <a href="#/tickets/{{ticket.nice_id}}">{{ticket.subject}}</a>' +
+            '    </li>{{/bookmarks}}' +
+            '  </ul>' +
+            '  {{^bookmarks}}None{{/bookmarks}}' +
+            '  <button class="btn bookmark">Bookmark This Ticket</button>' +
+            '</div>'
+    },
+
+    requests: {
+      bookmarks: {
+        url: '/api/v1/bookmarks.json'
+      },
+
+      add_bookmark: function() {
+        return {
+          url: '/api/v1/bookmarks.json',
+          type: 'POST',
+          data: {
+            ticket_id: this.deps.ticketID
+          }
+        };
+      }
+    },
+
+    dependencies: {
+      ticketID:       'Zendesk.currentTicket.id'
+    },
+
+    events: {
+      'bookmarks.done': function(e, data) {
+        this.sheet('bookmarks')
+          .render('main', { bookmarks: data.bookmarks })
+          .show();
+      },
+
+      'add_bookmark.success': function() {
+        this.services.notify('Bookmarked ticket #%@'.fmt(this.deps.ticketID));
+      },
+
+      'add_bookmark.fail': function() {
+        this.services.notify('Failed to bookmark ticket #%@'.fmt(this.deps.ticketID), 'error');
+      },
+
+      'click %default': function() {
+        this.request('bookmarks').perform();
+      },
+
+      'click .bookmark': function() {
+        this.request('add_bookmark').perform();
+      },
+
+      'add_bookmark.done': function(e, data) {
+        this.request('bookmarks').perform();
+      }
+    },
+
+    launch: function(host, config) {
+      this.request('bookmarks').perform();
+    }
+  });
+
+}());
