28c7dbdd08f8daa44dd6807ecf57469397de655b
Igor Justino de Souza <igor@zendesk.com>
2012-05-02 20:45:55 -0700

    Ensure extraction on all current pre-built apps

--- bookmarks/app.js
+++ bookmarks/app.js
@@ -2,24 +2,24 @@
 
   return ZendeskApps.defineApp(ZendeskApps.Site.TICKET_PROPERTIES, {
     appID: '/apps/01-bookmarks/versions/1.0.0',
-    name: 'Bookmarks',
+
     defaultSheet: 'loading',
 
     templates: {
       main: '<div class="bookmarks_app">' +
             '  <div data-sheet-name="loading" class="loading">' +
-            '    <h3>Bookmarks</h3><hr/>' +
-            '   Loading&hellip;' +
+            '    <h3>{{I18n.title}}</h3><hr/>' +
+            '   {{I18n.loading}}&hellip;' +
             '  </div>' +
             '  <div data-sheet-name="bookmarks">' +
-            '    <h3>Bookmarks</h3><hr/>' +
+            '    <h3>{{I18n.title}}</h3><hr/>' +
             '    <ul>' +
             '      {{#bookmarks}}<li>' +
             '        <a href="#/tickets/{{ticket.nice_id}}">{{ticket.subject}}</a>' +
             '      </li>{{/bookmarks}}' +
             '    </ul>' +
-            '    {{^bookmarks}}None{{/bookmarks}}' +
-            '    <button class="btn bookmark">Bookmark This Ticket</button>' +
+            '    {{^bookmarks}}{{I18n.none}}{{/bookmarks}}' +
+            '    <button class="btn bookmark">{{I18n.bookmark_this_ticket}}</button>' +
             '  </div>' +
             '</div>'
     },
