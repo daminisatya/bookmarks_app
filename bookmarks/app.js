372218671ded25d19821b009001a153d5f829a82
James A. Rosen <james@zendesk.com>
2012-05-22 10:29:08 -0700

    [Fix #29794471, #27423777] Bookmarks app: improvements
     * When a ticket is closed, deleted, or already bookmarked, don't show the
       "Bokomark this Ticket" button
     * Show a "delete" button next to each existing bookmark

--- bookmarks/app.js
+++ bookmarks/app.js
@@ -1,5 +1,7 @@
 (function(exports) {
 
+  var STATUS_CLOSED = 4;
+
   return ZendeskApps.defineApp(ZendeskApps.Site.TICKET_PROPERTIES, {
     appID: '/apps/01-bookmarks/versions/1.0.0',
 
@@ -18,11 +20,20 @@
             ticket_id: this.dependency('ticketID')
           }
         };
+      },
+
+      destroyBookmark: function() {
+        return {
+          url: helpers.fmt('/api/v1/bookmarks/%@.json', this.bookmarkToDestroy),
+          type: 'POST',
+          data: { _method: 'DELETE' }
+        };
       }
     },
 
     dependencies: {
       ticketID: 'ticket.id',
+      ticketStatus: 'ticket.status',
       ticketSubject: 'ticket.subject'
     },
 
@@ -30,32 +41,77 @@
       'app.activated': 'requestBookmarks',
 
       'fetchBookmarks.always': function(e, data) {
-        this.switchTo('list', { bookmarks: data.bookmarks });
+        this.renderBookmarks((data || {}).bookmarks);
       },
 
       'addBookmark.done': function() {
         services.notify(helpers.fmt('Bookmarked ticket #%@', this.dependency('ticketID')));
       },
-
       'addBookmark.fail': function() {
         services.notify(helpers.fmt('Failed to bookmark ticket #%@', this.dependency('ticketID')), 'error');
       },
 
-      'click %welcome': function() {
+      'click %welcome': function(event) {
+        event.preventDefault();
         this.ajax('fetchBookmarks');
       },
 
-      'click .bookmark': function() {
+      'click .bookmark': function(event) {
+        event.preventDefault();
         this.ajax('addBookmark');
+      },
+
+      'click .destroy': function(event) {
+        event.preventDefault();
+        this.bookmarkToDestroy = this.bookmarkID(event);
+        if (this.bookmarkToDestroy != null) { this.ajax('destroyBookmark'); }
       },
 
       'addBookmark.always': function(e, data) {
         this.ajax('fetchBookmarks');
+      },
+
+      'destroyBookmark.done': function() {
+        var idToDelete = this.bookmarkToDestroy;
+        this.renderBookmarks(_.reject(this.bookmarks, function(b) {
+          return b.id === idToDelete;
+        }));
       }
+    },
+
+    renderBookmarks: function(bookmarks) {
+      this.bookmarks = bookmarks;
+      this.switchTo('list', {
+        bookmarks: this.bookmarks,
+        ticketIsBookmarkable: this.ticketIsBookmarkable()
+      });
+    },
+
+    ticketIsBookmarkable: function() {
+      var status = this.dependency('ticketStatus') || 0;
+      if ( status >= STATUS_CLOSED ) { return false; }
+
+      var ticketID = this.dependency('ticketID'),
+          alreadyBookmarked = _.any(this.bookmarks, function(b) {
+            return b.ticket.nice_id === ticketID;
+          });
+
+      return !alreadyBookmarked;
     },
 
     requestBookmarks: function() {
       this.ajax('fetchBookmarks');
+    },
+
+    // Get the bookmark ID for a click event within a bookmark <li>
+    bookmarkID: function(event) {
+      return this.$(event.target)
+                 .closest('[data-bookmark-id]')
+                 .data('bookmark-id');
+    },
+
+    bookmarkLI: function(id) {
+      return this.$( helpers.fmt('li[data-bookmark-id="%@"]', id) );
     }
   });
 
